5a192bf32abcb8078ebbe77a54c382b91488c2b4
David Yachabach <yachabach@gmail.com>
2022-11-27 17:30:47 -0500

    feat: portfolio entity; can add money objects of the same currency

--- js/test_money.js
+++ js/test_money.js
@@ -15,6 +15,23 @@ class Money {
     }
 }
 
+class Portfolio {
+    constructor(){
+        this.moneys = [];
+    }
+
+    add(...moneys){
+        this.moneys = this.moneys.concat(moneys);
+    }
+
+    evaluate(currency){
+        let total = this.moneys.reduce( (sum, money) => {
+            return sum + money.amount;
+        }, 0)
+        return new Money(total, currency)
+    }
+}
+
 let fiveDollars = new Money(5, "USD");
 let tenDollars = new Money(10, "USD");
 assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
@@ -23,7 +40,12 @@ let tenEuros = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
 assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
 
-let originalMoney = new Money(4002, "KRW")
-let actualMoneyAfterDivision = originalMoney.divide(4)
-let expectedMoneyAfterDivition = new Money(1000.5, "KRW")
-assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivition)
+let originalMoney = new Money(4002, "KRW");
+let actualMoneyAfterDivision = originalMoney.divide(4);
+let expectedMoneyAfterDivition = new Money(1000.5, "KRW");
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivition);
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
